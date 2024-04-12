e790176ca7529ff36c81125f5ae6b73008ae66bc
Jim Hawkins <sjjhsjjhsjjh@gmail.com>
2024-04-12 11:27:04 +0100

    Finalise home page decommissioning by removing class file and vestigial references to PageHome
    Signed-off-by: Jim Hawkins <sjjhsjjhsjjh@gmail.com>

--- src/gui/pages/__init__.py
+++ src/gui/pages/__init__.py
@@ -1,9 +1,8 @@
 __all__ = [
-    'PageCursor', 'PageHome', 'PageKeyboard', 'PageSelectCamera'
-    , 'PageSelectGestures', 'PageAbout'
+    'PageCursor', 'PageKeyboard', 'PageSelectCamera', 'PageSelectGestures'
+    , 'PageAbout'
 ]
 from .page_cursor import PageCursor
-from .page_home import PageHome
 from .page_keyboard import PageKeyboard
 from .page_select_camera import PageSelectCamera
 from .page_select_gestures import PageSelectGestures
