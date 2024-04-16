ae63829f4fb3f1e71b26a965a8fd4e1ec1ac502d
Jim Hawkins <sjjhsjjhsjjh@gmail.com>
2024-04-16 15:51:56 +0100

    Add spaceless key-value pair
    Signed-off-by: Jim Hawkins <sjjhsjjhsjjh@gmail.com>

--- src/utils/readini.py
+++ src/utils/readini.py
@@ -23,8 +23,14 @@ if __name__ == "__main__":
     argumentParser = ArgumentParser(
         description=
         'Print a value from a .ini file in a key=value format for a GitHub'
-        ' workflow output.'
+        ' workflow output. Two pairs are printed, one guaranteed to have no'
+        ' whitespace.'
     )
+    argumentParser.add_argument(
+        "--no-space-suffix", dest='spacelessSuffix', type=str, metavar="SUFFIX"
+        , default="Spaceless", help=
+        'Suffix to add to the key in the pair with no whitespace.'
+        ' Default is "Spaceless".')
     argumentParser.add_argument(
         "path", metavar="File.ini", type=Path, help='Path of the .ini file.')
     argumentParser.add_argument(
@@ -40,9 +46,10 @@ if __name__ == "__main__":
         'Optional key to appear in the output.'
         ' Default is to use the same key that appears in the .ini file.')
     arguments = argumentParser.parse_args()
-    print('='.join((
-        arguments.key if arguments.alias is None else arguments.alias,
-        get_ini_value(arguments.path, arguments.section, arguments.key)
-        # '_'.join(get_ini_value(
-        #     arguments.path, arguments.section, arguments.key).split())
-    )))
+    value = get_ini_value(arguments.path, arguments.section, arguments.key)
+    key = arguments.key if arguments.alias is None else arguments.alias
+    for printKey, printValue in (
+        (key, value),
+        (key + arguments.spacelessSuffix, '_'.join(value.split()))
+    ):
+        print('='.join((printKey, printValue)))
