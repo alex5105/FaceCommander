55ba2adb7f7e0f1750c7864db0a3c862ee8d8b5c
Jim Hawkins <sjjhsjjhsjjh@gmail.com>
2024-04-18 15:00:43 +0100

    Change to use platformdirs modules to determine app data location, and refactor file-level code to be within the ConfigManager class getters, and rationalise initial profile load and profile directory scanning, and change name of file with current profile to current.json, and refactor profiles interface, and move some inline values to constants set at the top of the file, and eliminate os module and make more use of Path interfaces, and change some variable names to camel-case convention which is Python best practice, and tidy up imports
    Signed-off-by: Jim Hawkins <sjjhsjjhsjjh@gmail.com>

--- src/config_manager.py
+++ src/config_manager.py
@@ -1,182 +1,251 @@
+# Standard library imports.
+#
 import copy
-import json
 import logging
-import shutil
 import time
 import tkinter as tk
-import os
+#
+# JSON module.
+# https://docs.python.org/3/library/json.html
+import json
 #
 # Object oriented path handling.
 # https://docs.python.org/3/library/pathlib.html
 from pathlib import Path
-
+#
+# Module for high-level file operations.
+# https://docs.python.org/3/library/shutil.html
+import shutil
+#
+# PIP modules.
+#
+# https://platformdirs.readthedocs.io/en/latest/api.html
+from platformdirs import user_data_dir
+#
+# Local imports.
+#
 from src.singleton_meta import Singleton
 from src.task_killer import TaskKiller
 from src.utils.Trigger import Trigger
 from src.utils.readini import get_ini_value
 
-VERSION = get_ini_value(
-    Path(__file__).parents[1] / "assets" / "Version.ini"
-    , 'Release', 'VersionNumber'
-)
+APP_NAME = "FaceCommander"
+APP_AUTHOR = "AceCentre"
 
-DEFAULT_JSON = Path(f"C:/Users/{os.getlogin()}/FaceCommander/configs/default.json")
-BACKUP_PROFILE = Path(f"C:/Users/{os.getlogin()}/FaceCommander/configs/default")
+PROFILES_SUBDIRECTORY = ("configs",)
+CURRENT_PROFILE_FILE = PROFILES_SUBDIRECTORY + ("current.json",)
+CURSOR_FILENAME = "cursor.json"
+MOUSE_FILENAME = "mouse_bindings.json"
+KEYBOARD_FILENAME = "keyboard_bindings.json"
+BACKUP_PROFILE = "default"
+
+VERSION_INI_FILE = ("assets", "Version.ini")
+VERSION_INI_SECTION = "Release"
+VERSION_INI_KEY = "VersionNumber"
 
 logger = logging.getLogger("ConfigManager")
 
-config_dir = f"C:/Users/{os.getlogin()}/FaceCommander/configs/"
-default_dir = os.path.join(config_dir, "default")
-
-# Create the main config directory if it doesn't exist
-if not os.path.isdir(config_dir):
-    os.makedirs(config_dir, exist_ok=True)
-    shutil.copytree("configs", config_dir, dirs_exist_ok=True)
-
-# Create the default directory inside the config directory if it doesn't exist
-if not os.path.isdir(default_dir):
-    os.mkdir(default_dir)
-
-if not os.path.isdir(f"C:/Users/{os.getlogin()}/FaceCommander/configs/"):
-    shutil.copytree("configs", f"C:/Users/{os.getlogin()}/FaceCommander/configs/")
-    os.mkdir(f"C:/Users/{os.getlogin()}/FaceCommander/configs/")
-
-if not os.path.isdir(f"C:/Users/{os.getlogin()}/FaceCommander/configs/default"):
-    os.mkdir(f"C:/Users/{os.getlogin()}/FaceCommander/configs/default")
-
-
 class ConfigManager(metaclass=Singleton):
     def __init__(self):
-        self.temp_keyboard_bindings = None
-        self.temp_mouse_bindings = None
-        self.temp_config = None
+        self._appInstallationRoot = Path(__file__).parents[1]
+        logger.info(
+            "Initialising ConfigManager singleton."
+            f' Installation root "{self.appInstallationRoot}".')
+        self._appDataRoot = None
+        self._profilesDirectory = None
+        self._currentProfilePath = None
+        self._version = None
+        self._profileNames = None
+
+        self.tempKeyboardBindings = None
+        self.tempMouseBindings = None
+        self.tempConfig = None
         self.keyboard_bindings = None
         self.mouse_bindings = None
-        logger.info("Initialize ConfigManager singleton")
-        self.version = VERSION
+
         self.unsave_configs = False
         self.unsave_mouse_bindings = False
         self.unsave_keyboard_bindings = False
         self.config = None
 
         # Load config
-        self.current_profile_path = None
+        self.currentProfileDirectory = None
         self.current_profile_name = tk.StringVar()
         self.is_started = False
+    
+    @property
+    def appInstallationRoot(self):
+        return self._appInstallationRoot
+    
+    @property
+    def currentProfilePath(self):
+        if self._currentProfilePath is None:
+            self._currentProfilePath = Path(
+                self.appDataRoot, *CURRENT_PROFILE_FILE)
+        return self._currentProfilePath
+    
+    @property
+    def appDataRoot(self):
+        if self._appDataRoot is None:
+            self._appDataRoot = Path(user_data_dir(
+                appname=APP_NAME, appauthor=APP_AUTHOR))
+        return self._appDataRoot
+    
+    @property
+    def version(self):
+        if self._version is None:
+            self._version = get_ini_value(
+                Path(self.appInstallationRoot, *VERSION_INI_FILE),
+                VERSION_INI_SECTION, VERSION_INI_KEY)
+        return self._version
 
-        self.profiles = self.list_profile()
+    def _get_profiles_directory(self, *name):
+        if self._profilesDirectory is None:
+            self._profilesDirectory = Path(
+                self.appDataRoot, *PROFILES_SUBDIRECTORY)
+        path = self._profilesDirectory
+        if path.exists():
+            if not path.is_dir():
+                raise RuntimeError(
+                    "Non-directory found where profiles directory is expected."
+                    f' {path}')
+        else:
+            builtIn = Path(self.appInstallationRoot, *PROFILES_SUBDIRECTORY)
+            logger.info(
+                "Creating profiles directory by copying built-in profiles."
+                f' Profiles directory "{path}".'
+                f' Built-in profiles "{builtIn}".')
+            shutil.copytree(builtIn, path)
+        return Path(path, *name)
 
     def start(self):
         if not self.is_started:
             logger.info("Start ConfigManager singleton")
-            if not DEFAULT_JSON.is_file():
-                logger.critical(f"Missing {DEFAULT_JSON}, exit program...")
-                TaskKiller().exit()
-
+            loaded = None
             try:
-                with open(DEFAULT_JSON) as f:
-                    self.load_profile(json.load(f)["default"])
-            except Exception as e:
-                logging.error(e)
+                with self.currentProfilePath.open() as file:
+                    self.load_profile(json.load(file)["default"])
+                loaded = True
+            except Exception as exception:
+                loaded = False
+                logging.error(exception)
+            if not loaded:
                 logging.error(
-                    f"Failed to load default profile {DEFAULT_JSON}, using first profile instead."
-                )
-                self.load_profile(self.list_profile()[0])
+                    "Failed to load profile named in"
+                    f' "{self.currentProfilePath}".')
+                for name in self.profileNames:
+                    try:
+                        self.load_profile(name)
+                        loaded = True
+                        logging.error(f'Loaded profile as fall-back "{name}".')
+                        break
+                    except Exception as exception:
+                        logging.error(exception)
+                        logging.error(
+                            f'Failed to fall back to profile "{name}".')
+            if not loaded:
+                raise RuntimeError("Failed to load any profile.")
             self.is_started = True
 
-    def list_profile(self) -> list:
-        profile_dirs = []
-        for dir in DEFAULT_JSON.parent.glob("*"):
-            if dir.is_dir():
-                profile_dirs.append(dir.name)
-        logger.info(profile_dirs)
-        return profile_dirs
+    @property
+    def profileNames(self):
+        if self._profileNames is None:
+            self._profileNames = tuple(
+                child.name for child in self._get_profiles_directory().iterdir()
+                if child.is_dir()
+            )
+            logger.info(f'Profiles discovered {self._profileNames}.')
+        return self._profileNames
 
-    def remove_profile(self, profile_name):
-        logger.info(f"Remove profile {profile_name}")
-        shutil.rmtree(Path(DEFAULT_JSON.parent, profile_name))
-        self.profiles.remove(profile_name)
-        logger.info(f"Current profiles: {self.profiles}")
+    def remove_profile(self, name):
+        logger.info(f'Remove profile "{name}".')
+        shutil.rmtree(self._get_profiles_directory(name))
+        # Next line will force the getter to rediscover profiles.
+        self._profileNames = None
+        logger.info(f"Remaining profiles {self.profileNames}.")
 
     def add_profile(self):
         # Random name base on local timestamp
-        new_profile_name = "profile_z" + str(hex(int(time.time() * 1000)))[2:]
-        logger.info(f"Add profile {new_profile_name}")
-        shutil.copytree(BACKUP_PROFILE, Path(DEFAULT_JSON.parent, new_profile_name))
-        self.profiles.append(new_profile_name)
-        logger.info(f"Current profiles: {self.profiles}")
-
-    def rename_profile(self, old_profile_name, new_profile_name):
-        logger.info(f"Rename profile {old_profile_name} to {new_profile_name}")
-        shutil.move(
-            Path(DEFAULT_JSON.parent, old_profile_name),
-            Path(DEFAULT_JSON.parent, new_profile_name),
+        name = "profile_z" + str(hex(int(time.time() * 1000)))[2:]
+        logger.info(f'Adding profile "{name}".')
+        shutil.copytree(
+            self._get_profiles_directory(BACKUP_PROFILE),
+            self._get_profiles_directory(name)
         )
-        self.profiles.remove(old_profile_name)
-        self.profiles.append(new_profile_name)
+        # Next line will force the getter to rediscover profiles.
+        self._profileNames = None
+        logger.info(f"Profiles after addition {self.profileNames}.")
 
-        if self.current_profile_name.get() == old_profile_name:
-            self.current_profile_name.set(new_profile_name)
+    def rename_profile(self, oldName, newName):
+        logger.info(f'Rename "{oldName}" to "{newName}".')
+        (self._get_profiles_directory(oldName)).rename(
+            self._get_profiles_directory(newName)
+        )
+        # Next line will force the getter to rediscover profiles.
+        self._profileNames = None
+        logger.info(f"Profiles after rename {self.profileNames}.")
 
-    def load_profile(self, profile_name: str):
-        profile_path = Path(DEFAULT_JSON.parent, profile_name)
-        logger.info(f"Loading profile: {profile_path}")
+        if self.current_profile_name.get() == oldName:
+            self.current_profile_name.set(newName)
 
-        cursor_config_file = Path(profile_path, "cursor.json")
-        mouse_bindings_file = Path(profile_path, "mouse_bindings.json")
-        keyboard_bindings_file = Path(profile_path, "keyboard_bindings.json")
+    def load_profile(self, name: str):
+        profileDirectory = self._get_profiles_directory(name)
+        logger.info(f'Loading profile from "{profileDirectory}"')
 
-        if (
-            (not cursor_config_file.is_file())
-            or (not mouse_bindings_file.is_file())
-            or (not keyboard_bindings_file.is_file())
-        ):
-            logger.critical(
-                f"{profile_path.as_posix()} Invalid configuration files or missing files, exit program..."
-            )
-            raise FileNotFoundError
+        cursorPath = profileDirectory / CURSOR_FILENAME
+        mousePath =  profileDirectory / MOUSE_FILENAME
+        keyboardPath = profileDirectory / KEYBOARD_FILENAME
+        missing = tuple(
+            path for path in (cursorPath, mousePath, keyboardPath)
+            if not path.is_file()
+        )
+        if len(missing) > 0:
+            logger.critical(f'Configuration is invalid "{name}".')
+            for path in missing:
+                logger.critical(f'Missing configuration file "{path}".')
+            raise FileNotFoundError(missing)
 
         # Load cursor config
-        with open(cursor_config_file) as f:
-            self.config = json.load(f)
+        with cursorPath.open() as file:
+            self.config = json.load(file)
 
         # Load mouse bindings
-        with open(mouse_bindings_file) as f:
-            self.mouse_bindings = json.load(f)
+        with mousePath.open() as file:
+            self.mouse_bindings = json.load(file)
 
         # Load keyboard bindings
-        with open(keyboard_bindings_file) as f:
-            self.keyboard_bindings = json.load(f)
+        with keyboardPath.open() as file:
+            self.keyboard_bindings = json.load(file)
 
-        self.temp_config = copy.deepcopy(self.config)
-        self.temp_mouse_bindings = copy.deepcopy(self.mouse_bindings)
-        self.temp_keyboard_bindings = copy.deepcopy(self.keyboard_bindings)
+        self.tempConfig = copy.deepcopy(self.config)
+        self.tempMouseBindings = copy.deepcopy(self.mouse_bindings)
+        self.tempKeyboardBindings = copy.deepcopy(self.keyboard_bindings)
 
-        self.current_profile_path = profile_path
-        self.current_profile_name.set(profile_name)
+        self.currentProfileDirectory = profileDirectory
+        self.current_profile_name.set(name)
 
-    def switch_profile(self, profile_name: str):
-        logger.info(f"Switching to profile: {profile_name}")
-        self.load_profile(profile_name)
-        with open(DEFAULT_JSON, "w") as f:
-            json.dump({"default": profile_name}, f)
+    def switch_profile(self, name: str):
+        logger.info(f'Switching to profile "{name}"')
+        self.load_profile(name)
+        with self.currentProfilePath.open("w") as file:
+            json.dump({"default": name}, file)
 
     # ------------------------------- BASIC CONFIG ------------------------------- #
 
     def set_temp_config(self, field: str, value):
         logger.info(f"Setting {field} to {value}")
-        self.temp_config[field] = value
+        self.tempConfig[field] = value
         self.unsave_configs = True
 
     def write_config_file(self):
-        cursor_config_file = Path(self.current_profile_path, "cursor.json")
-        logger.info(f"Writing config file {cursor_config_file}")
-        with open(cursor_config_file, "w") as f:
-            json.dump(self.config, f, indent=4, separators=(", ", ": "))
+        cursorPath = Path(self.currentProfileDirectory, CURSOR_FILENAME)
+        logger.info(f"Writing config file {cursorPath}")
+        with cursorPath.open('w') as file:
+            json.dump(self.config, file, indent=4, separators=(', ', ': '))
 
     def apply_config(self):
         logger.info("Applying config")
-        self.config = copy.deepcopy(self.temp_config)
+        self.config = copy.deepcopy(self.tempConfig)
         self.write_config_file()
         self.unsave_configs = False
 
@@ -198,37 +267,34 @@ class ConfigManager(metaclass=Singleton):
         self.remove_temp_mouse_binding(device, action)
 
         # Assign
-        self.temp_mouse_bindings[gesture] = [
-            device,
-            action,
-            float(threshold),
-            trigger.value,
+        self.tempMouseBindings[gesture] = [
+            device, action, float(threshold), trigger.value
         ]
         self.unsave_mouse_bindings = True
 
     def remove_temp_mouse_binding(self, device: str, action: str):
         logger.info(f"remove_temp_mouse_binding for device: {device}, key: {action}")
         out_keybindings = {}
-        for key, vals in self.temp_mouse_bindings.items():
+        for key, vals in self.tempMouseBindings.items():
             if (device == vals[0]) and (action == vals[1]):
                 continue
             out_keybindings[key] = vals
-        self.temp_mouse_bindings = out_keybindings
+        self.tempMouseBindings = out_keybindings
         self.unsave_mouse_bindings = True
 
     def apply_mouse_bindings(self):
         logger.info("Applying keybindings")
-        self.mouse_bindings = copy.deepcopy(self.temp_mouse_bindings)
+        self.mouse_bindings = copy.deepcopy(self.tempMouseBindings)
         self.write_mouse_bindings_file()
         self.unsave_mouse_bindings = False
 
     def write_mouse_bindings_file(self):
-        mouse_bindings_file = Path(self.current_profile_path, "mouse_bindings.json")
-        logger.info(f"Writing keybindings file {mouse_bindings_file}")
+        mousePath = Path(self.currentProfileDirectory, MOUSE_FILENAME)
+        logger.info(f"Writing keybindings file {mousePath}")
 
-        with open(mouse_bindings_file, "w") as f:
+        with mousePath.open('w') as file:
             out_json = dict(sorted(self.mouse_bindings.items()))
-            json.dump(out_json, f, indent=4, separators=(", ", ": "))
+            json.dump(out_json, file, indent=4, separators=(', ', ': '))
 
     # ------------------------------ KEYBOARD BINDINGS CONFIG ----------------------------- #
 
@@ -253,11 +319,9 @@ class ConfigManager(metaclass=Singleton):
         self.remove_temp_keyboard_binding(device, key_action, gesture)
 
         # Assign
-        self.temp_keyboard_bindings[gesture] = [
-            device,
-            key_action,
-            float(threshold),
-            trigger.value,
+        self.tempKeyboardBindings[gesture] = [
+            device, key_action,
+            float(threshold), trigger.value
         ]
         self.unsave_keyboard_bindings = True
 
@@ -271,7 +335,7 @@ class ConfigManager(metaclass=Singleton):
         )
 
         out_keybindings = {}
-        for ges, vals in self.temp_keyboard_bindings.items():
+        for ges, vals in self.tempKeyboardBindings.items():
             if gesture == ges:
                 continue
             if key_action == vals[1]:
@@ -279,7 +343,7 @@ class ConfigManager(metaclass=Singleton):
 
             out_keybindings[ges] = vals
 
-        self.temp_keyboard_bindings = out_keybindings
+        self.tempKeyboardBindings = out_keybindings
 
         self.unsave_keyboard_bindings = True
         return
@@ -287,19 +351,17 @@ class ConfigManager(metaclass=Singleton):
     def apply_keyboard_bindings(self):
         logger.info("Applying keyboard bindings")
 
-        self.keyboard_bindings = copy.deepcopy(self.temp_keyboard_bindings)
+        self.keyboard_bindings = copy.deepcopy(self.tempKeyboardBindings)
         self.write_keyboard_bindings_file()
         self.unsave_keyboard_bindings = False
 
     def write_keyboard_bindings_file(self):
-        keyboard_bindings_file = Path(
-            self.current_profile_path, "keyboard_bindings.json"
-        )
-        logger.info(f"Writing keyboard bindings file {keyboard_bindings_file}")
+        keyboardPath = Path(self.currentProfileDirectory, KEYBOARD_FILENAME)
+        logger.info(f"Writing keyboard bindings file {keyboardPath}")
 
-        with open(keyboard_bindings_file, "w") as f:
+        with keyboardPath.open('w') as file:
             out_json = dict(sorted(self.keyboard_bindings.items()))
-            json.dump(out_json, f, indent=4, separators=(", ", ": "))
+            json.dump(out_json, file, indent=4, separators=(', ', ': '))
 
     # ---------------------------------------------------------------------------- #
     def apply_all(self):
