a1b370dd73e426b332108beadc44eaa1405c04ba
Jim Hawkins <sjjhsjjhsjjh@gmail.com>
2024-04-25 23:00:02 +0100

    Add in-progress improved tkinter Text building implementation for safekeeping
    Signed-off-by: Jim Hawkins <sjjhsjjhsjjh@gmail.com>

--- src/gui/pages/page_about.py
+++ src/gui/pages/page_about.py
@@ -11,9 +11,14 @@ import logging
 # TOTH  
 # -   https://www.pythontutorial.net/tkinter/tkinter-event-binding/
 # -   https://www.pythontutorial.net/tkinter/tkinter-place/
+from tkinter import END
 from tkinter.ttk import Label
 from tkinter.scrolledtext import ScrolledText
 from tkinter.font import Font #, families as font_families
+#
+# Type hints module.
+# https://docs.python.org/3/library/typing.html#typing.NamedTuple
+from typing import NamedTuple, Callable
 #
 # Browser launcher module.
 # https://docs.python.org/3/library/webbrowser.html
@@ -29,63 +34,10 @@ from src.gui.frames.safe_disposable_frame import SafeDisposableFrame
 from src.update_manager import UpdateManager
 
 logger = logging.getLogger("PageAbout")
-def log_path(description, path): logger.info(" ".join((
-    description, "".join(('"', str(path), '"'))
-    , "Exists." if path.exists() else "Doesn't exist."
-)))
-
-# Jim initially was using Python lambda expressions for the event handlers. That
-# seemed to result in all tag_bind() calls being overridden to whichever was the
-# last one called. So now the tags are set up here, outside the class, and
-# lambda expressions aren't used.
-addressTags = {}
-def add_address_tag(pageAbout, address):
-    def _enter(event): pageAbout.hover_enter(address, event)
-    def _leave(event): pageAbout.hover_leave(address, event)
-    def _click(event): pageAbout.open_in_browser(address, event)
-
-    addressTag = {
-        'tag': f'address{len(addressTags)}',
-        'enter': _enter,
-        'leave': _leave,
-        'click': _click
-    }
-    tagText = addressTag['tag']
-
-    pageAbout.text.tag_configure(tagText)
-
-    # TOTH using tag_bind. https://stackoverflow.com/a/65733556/7657675  
-    # TOTH list of events that makes clear they have to be in angle brackets.
-    # https://stackoverflow.com/a/32289245/7657675
-    #
-    # -   <Enter> is triggered when the pointer hovers here.
-    # -   <Leave> is triggered when the pointer stops hovering here.
-    # -   <1> is triggered when this is clicked. It seems to be a shorthand for
-    #     button-1.
-    pageAbout.text.tag_bind(tagText, "<Enter>", addressTag['enter'])
-    pageAbout.text.tag_bind(tagText, "<Leave>", addressTag['leave'])
-    pageAbout.text.tag_bind(tagText, "<1>", addressTag['click'])
-
-    addressTags[address] = addressTag
-    return addressTag
-
-def add_address_tags(pageAbout, addresses):
-    # TOTH Underlining https://stackoverflow.com/a/44890599/7657675
-    pageAbout.text.tag_configure("link", underline=True, foreground="#0000EE")
-
-    for address in addresses:
-        add_address_tag(pageAbout, address)
-
-    return addressTags
-
-def tags_for(address):
-    try:
-        return ("link", addressTags[address]['tag'])
-    except KeyError as keyError:
-        raise ValueError(
-            "Address hasn't been registered."
-            f' Add add_address_tag(pageAbout,"{address}")'
-        ) from keyError
+# def log_path(description, path): logger.info(" ".join((
+#     description, "".join(('"', str(path), '"'))
+#     , "Exists." if path.exists() else "Doesn't exist."
+# )))
 
 class PageAbout(SafeDisposableFrame):
     hoverCursor = "hand2"
@@ -120,17 +72,19 @@ class PageAbout(SafeDisposableFrame):
         # Create tags for styling the page content.
         self.text.tag_configure("h1", font=font24)
         self.text.tag_configure("h2", font=font18)
+        # TOTH Underlining https://stackoverflow.com/a/44890599/7657675
+        self.text.tag_configure(
+            "link", underline=True, foreground="#0000EE")
 
-        # Create a tag for every address in the about page. That seems to be the
-        # only way that event handlers can be bound.
-        add_address_tags(self, (
-            "https://acecentre.org.uk",
-            App().repositoryURL,
-            "https://www.flaticon.com/free-icons/eye",
-            "https://github.com/acidcoke/Grimassist/",
-            "https://github.com/google/project-gameface"
-        ) )
-        logger.info(f'addressTags{addressTags}')
+        # # Create a tag for every address in the about page. That seems to be the
+        # # only way that event handlers can be bound.
+        # add_address_tags(self, (
+        #     "https://acecentre.org.uk",
+        #     App().repositoryURL,
+        #     "https://www.flaticon.com/free-icons/eye",
+        #     "https://github.com/acidcoke/Grimassist/",
+        #     "https://github.com/google/project-gameface"
+        # ) )
 
         # Fix the widget to the top of the frame. The height is set later, in
         # the configuration change handler. That handler will be invoked once
@@ -141,31 +95,38 @@ class PageAbout(SafeDisposableFrame):
         # The argument tail is an alternating sequence of texts and tag lists.
         # If the text is to be set in the default style then an empty tag list
         # () is given.
-        self.text.insert("1.0"
-        , f"About {App().name}\n", "h1"
-        , "Control and move the pointer using head movements and facial"
-        " gestures.\nDisclaimer: This software isn't intended for medical"
-        f" use.\n{App().name} is an ", (),
-        "Open Source project", tags_for(App().repositoryURL),
-        " developed by the Ace Centre. Visit ", (),
-        "our website", tags_for("https://acecentre.org.uk"),
-        " to find out more about how we provide support for people with"
-        " complex communications difficulties.\n", ()
-        , "Releases\n", "h2"
-        , f"Version {App().version}\n", ()
-        , f"Last check for updates {self.last_fetch()}.\n", ()
-        , "Attribution\n", "h2"
-        , "Blink graphics in the user interface are based on ", ()
-        , "Eye icons created by Kiranshastry - Flaticon", tags_for(
-            "https://www.flaticon.com/free-icons/eye")
-        , ".\nThis software was based on ", ()
-        , "Grimassist", tags_for(
-            "https://github.com/acidcoke/Grimassist/")
-        , ", itself based on ", ()
-        , "Google GameFace", tags_for(
-            "https://github.com/google/project-gameface")
-        , ".", ()
+        self._spanned = SpannedText(self
+        ).paragraph(f"About {App().name}", "h1"
+        ).span(f"""\
+Control and move the pointer using head movements and facial gestures.
+Disclaimer: This software isn't intended for medical use.
+{App().name} is an """
+        ).link("Open Source project", App().repositoryURL
+        ).span(" developed by the Ace Centre. Visit "
+        ).link("our website", "https://acecentre.org.uk"
+        ).paragraph(
+            " to find out more about how we provide support for people with"
+            " complex communications difficulties."
+        ).paragraph("Releases", "h2"
+        ).paragraph(f"Version {App().version}"
         )
+
+
+
+
+        # , f"Last check for updates {self.last_fetch()}.\n", ()
+        # , "Attribution\n", "h2"
+        # , "Blink graphics in the user interface are based on ", ()
+        # , "Eye icons created by Kiranshastry - Flaticon", tags_for(
+        #     "https://www.flaticon.com/free-icons/eye")
+        # , ".\nThis software was based on ", ()
+        # , "Grimassist", tags_for(
+        #     "https://github.com/acidcoke/Grimassist/")
+        # , ", itself based on ", ()
+        # , "Google GameFace", tags_for(
+        #     "https://github.com/google/project-gameface")
+        # , ".", ()
+        # )
         self.text.configure(state='disabled')
 
         # When the pointer hovers over a link, change it to a hand. It might
@@ -237,3 +198,52 @@ class PageAbout(SafeDisposableFrame):
 
     def refresh_profile(self):
         pass
+
+class Anchor(NamedTuple):
+    tag:str
+    address:str
+    enter:Callable
+    leave:Callable
+    click:Callable
+
+class SpannedText:
+    def __init__(self, pageAbout):
+        self._page = pageAbout
+        self._anchors = {}
+
+        self._linkTag = "link"
+    
+    def span(self, text, *tags):
+        self._page.text.insert(END, text, tags)
+        return self
+    
+    def paragraph(self, text, *tags):
+        return self.span(text + "\n", *tags)
+    
+    def link(self, text, address):
+        # Jim initially was using Python lambda expressions for the event
+        # handlers. That seemed to result in all tag_bind() calls being
+        # overridden to whichever was the last one called. So now lambda
+        # expressions aren't used.
+        def _enter(event): self._page.hover_enter(address, event)
+        def _leave(event): self._page.hover_leave(address, event)
+        def _click(event): self._page.open_in_browser(address, event)
+
+        anchor = Anchor(
+            f'address{len(self._anchors)}', address, _enter, _leave, _click)
+        self._page.text.tag_configure(anchor.tag)
+
+        # TOTH using tag_bind. https://stackoverflow.com/a/65733556/7657675  
+        # TOTH list of events that makes clear they have to be in angle
+        # brackets. https://stackoverflow.com/a/32289245/7657675
+        #
+        # -   <Enter> is triggered when the pointer hovers here.
+        # -   <Leave> is triggered when the pointer stops hovering here.
+        # -   <1> is triggered when this is clicked. It seems to be a shorthand
+        #     for button-1.
+        self._page.text.tag_bind(anchor.tag, "<Enter>", anchor.enter)
+        self._page.text.tag_bind(anchor.tag, "<Leave>", anchor.leave)
+        self._page.text.tag_bind(anchor.tag, "<1>", anchor.click)
+
+        self._anchors[address] = anchor
+        return self.span(text, self._linkTag, anchor.tag)
